f83916d866e716c9b0d5254ed0977609ead94278
claridiva2000 <claridiva2000@gmail.com>
2019-03-09 16:55:02 -0600

    styling and images done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Pictures from "./components/Pictures";
 import Characters from "./components/Characters"
 import './App.css';
 
@@ -7,7 +8,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      img:""
     };
   }
 
@@ -33,15 +35,20 @@ class App extends Component {
 
   render() {
     return (
+
+     
       <div className="App">
-        <h1 className="Header">React Wars</h1>
-       
+      <h1 className="Header">React Wars</h1>
+        <div className="container">
         <div className="char-list">
         {this.state.starwarsChars.map(star =>(
           <Characters star={star}/>
         ))}
-        
         </div>
+        <div className="images">
+        <Pictures />
+        </div>
+      </div>
       </div>
     );
   }
--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -8,9 +8,9 @@ function Characters(props){
 {/* <img src="https://vignette.wikia.nocookie.net/lego/images/6/6e/29233146812_13b19275a0_b.jpg/revision/latest?cb=20160901112919"></img> */}
 <div className="char-info">
 <h2>{props.star.name}</h2>
-<p><strong>Birth Year:</strong> {props.star.birth_year}</p>
-<p><strong>gender:</strong>{props.star.gender} </p>
-<p><strong>Height:</strong>{props.star.height} cm.</p>
+<p><strong>Birth Year :</strong> {props.star.birth_year}</p>
+<p><strong>gender : </strong>{props.star.gender} </p>
+<p><strong>Height : </strong>{props.star.height} cm.</p>
 </div>
 </div>
   );
